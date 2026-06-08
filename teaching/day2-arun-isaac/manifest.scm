5c121d31c0d522f4db1461404481487d3e6f83b1
Arun Isaac <arunisaac@systemreboot.net>
2026-06-08 08:11:34 +0100

    Use ccwl 0.5.0.

--- teaching/day2-arun-isaac/manifest.scm
+++ teaching/day2-arun-isaac/manifest.scm
@@ -1,10 +1,11 @@
-(use-modules (guix git-download)
+(use-modules (guix download)
+             (guix git-download)
              (guix packages)
-             ((gnu packages bioinformatics) #:select (ccwl))
              ((gnu packages bioinformatics)
-              #:select (ravanan) #:prefix guix:)
+              #:select (ccwl ravanan) #:prefix guix:)
              ((gnu packages graphviz) #:select (graphviz))
-             ((gnu packages guile) #:select (guile-3.0-latest)))
+             ((gnu packages guile) #:select (guile-3.0-latest))
+             ((gnu packages guile-xyz) #:select (guile-run64)))
 
 (define-public ravanan
   (let ((commit "1f5000ad6ff98278bf638ff0176ddfd5bf8933cf"))
@@ -24,6 +25,23 @@
       (inputs
        (modify-inputs (package-inputs guix:ravanan)
                       (replace "guile" guile-3.0-latest))))))
+
+(define-public ccwl
+  (package
+    (inherit guix:ccwl)
+    (name "ccwl")
+    (version "0.5.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://ccwl.systemreboot.net/releases/ccwl-"
+                           version ".tar.lz"))
+       (sha256
+        (base32
+         "1pna61cqhhg69qpa682x911x85nlin1wpp29naqzq3r0s951qxi6"))))
+    (native-inputs
+     (modify-inputs (package-native-inputs guix:ccwl)
+       (prepend guile-run64)))))
 
 (packages->manifest
  (list ccwl graphviz ravanan))
